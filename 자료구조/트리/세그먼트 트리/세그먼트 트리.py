066a49a79360361e0314c56a5bad9593fbf54205
byundojin <byundojin0216@gmail.com>
2023-11-01 21:07:20 +0900

    23/11/01

---
+++ 자료구조/트리/세그먼트 트리/세그먼트 트리.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, data) -> None:
+        self.data = data
+
+    def __repr__(self) -> str:
+        return f"Node:{self.data}"
+    
+class SegTree:
+    def __init__(self) -> None:
+        self.root:Node = None
+        self.point:Node = None
+
+    def add_node(self, node:Node):
+        self.
