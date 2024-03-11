5ef9007904d23d4cb545b6535ca1b003caba2043
byundojin <103907292+byundojin@users.noreply.github.com>
2024-03-11 16:44:06 +0900

    24/03/11

--- python lab/deco.py
+++ python lab/deco.py
@@ -1,23 +1,35 @@
-def a(func):
-    def _a():
-        print("a!!!")
-    return _a
+class Event():
+    events = {}
 
-def a1(func):
-    def _a():
-        print("a!!!")
-        func()
-    return _a
+    def add_event(str):
+        def wrapper(func):
+            print("add event", str)
+            if str in Event.events:
+                Event.events[str].append(func)
+            else:
+                Event.events[str] = [func]
+            return func
+        return wrapper
+    
+    def call(str):
+        if str in Event.events:
+            for func in Event.events[str]:
+                print("call", func.__name__)
+                func()
+        else:
+            raise f"{str} is not set event"
 
-def a2(func):
-    def _a():
-        print("a!!!")
-        return func
-    return _a()
+@Event.add_event("p1")
+def func_p1_1():
+    print("hello p1 1")
+@Event.add_event("p1")
+def func_p1_2():
+    print("hello p1 2")
+@Event.add_event("p2")
+def func_p2_1():
+    print("hello p2 1")
 
-@a2
-def b():
-    print("b!!!")
+Event.call("p1")
 
-b()
-
+        
+        
